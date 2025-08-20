d25e6e171f4565cdc94a952171a1abc13edf3c25
jroapuniacc <jorge.roap@uniacc.edu>
2025-08-20 12:56:23 -0400

    Clase02

--- Assets/00_Scripts/Interactions.cs
+++ Assets/00_Scripts/Interactions.cs
@@ -2,19 +2,40 @@ using System;
 using UnityEngine;
 
 public class Interactions : MonoBehaviour
-{
-        public bool key1 = false;
-        
+{       
+        [Header("Llaves")]
+        [Tooltip("Artistas, estas son las llaves del jugador")]
+        [SerializeField] private bool key1 = false;
+        [Tooltip("Artistas, estas son la llave 2 del NPC")]
+        [SerializeField] private bool key2 = false;
+
+        public GameObject door;
+        private Animator animatorDoor;
+
+        private void Start()
+        {
+                animatorDoor = door.GetComponent<Animator>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-                if (other.CompareTag("NPC1"))
-                { 
-                        key1 = true;
-                }
-
-                if (other.CompareTag("Door"))
+                switch (other.tag)
                 {
-                        Debug.Log("Puerta");
+                        case "NPC1": 
+                                key1 = true;
+                                Debug.Log("Obtuve la llave 1");
+                                break;
+                        case "NPC2":
+                                key2 = true;
+                                Debug.Log("Obtuve la llave 2");
+                                break;
+                        case "Door":
+                                Debug.Log("Abrí la puerta");
+                                animatorDoor.SetBool("Anim_door", true);
+                                break;
+                        default:
+                                Debug.Log("Consigue las llaves");
+                                break;
                 }
         }
 }
