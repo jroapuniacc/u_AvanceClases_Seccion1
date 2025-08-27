ebdc11f4077b699d833d5cb407aeed27a04e9f07
jroapuniacc <jorge.roap@uniacc.edu>
2025-08-27 12:05:52 -0400

    Clase03

--- Assets/00_Scripts/Interactions.cs
+++ Assets/00_Scripts/Interactions.cs
@@ -9,8 +9,16 @@ public class Interactions : MonoBehaviour
         [Tooltip("Artistas, estas son la llave 2 del NPC")]
         [SerializeField] private bool key2 = false;
 
-        public GameObject door;
-        private Animator animatorDoor;
+        [Header("Door")]
+        [SerializeField] private GameObject door; // Es el GO del pivotDoor
+        private Animator animatorDoor; // Componente del Pivot Door
+        private bool isOpenDoor = false;
+        
+        
+        [Header("Health")]
+        [SerializeField] private float health = 100f;
+        [SerializeField] private int healthDecrease = 5;
+        [SerializeField] private int healthIncrease = 10;
 
         private void Start()
         {
@@ -30,14 +38,56 @@ public class Interactions : MonoBehaviour
                                 Debug.Log("Obtuve la llave 2");
                                 break;
                         case "Door":
-                                Debug.Log("Abrí la puerta");
-                                animatorDoor.SetBool("Anim_door", true);
+                                if (key1 || key2)
+                                {
+                                        Debug.Log("Abrí la puerta");
+                                        animatorDoor.SetBool("Anim_door", true);
+                                        isOpenDoor = true;
+                                }
                                 break;
-                        default:
-                                Debug.Log("Consigue las llaves");
+                       
+                }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+                switch (other.tag)
+                {
+                        case "ZoneDanger":
+                                // Salud menos el daño
+                                // heatlh - healthdecrease
+                                if (isOpenDoor)
+                                {
+                                        health -= healthDecrease * Time.deltaTime;
+                                        Debug.Log("Mi vida está en " + health); // Concatenar   
+                                }
+                                
+                                        
+                                         
+                                
+                                break; 
+                                
+                        case "ZoneHealth":
+                                if (isOpenDoor)
+                                {
+                                        health += healthIncrease * Time.deltaTime;
+                                        Debug.Log("Mida vida está en " + health);    
+                                }
+                                break;  
+                }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+                switch (other.tag)
+                {
+                        case "Door":
+                                Debug.Log("Salí del collider Door");
+                                animatorDoor.SetBool("Anim_door", false);
                                 break;
                 }
         }
+        
 }
     
     
